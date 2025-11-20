5312333e2c4d4b1a6fb416ca8047cf0a6f7b236e
EliasdsAI <jedantas64@gmail.com>
2025-11-19 21:45:21 -0300

    Add files via upload

---
+++ Lista.py
@@ -0,0 +1,11 @@
+lista1 = []
+lista1.append("Leste")
+print(lista1)
+
+lista1.append("Norte")
+lista1.append("Sul")
+print(lista1)
+
+print("indice 0: " + str(lista1[0]))
+print("indice 1: " + str(lista1[1]))
+print("indice 2: " + str(lista1[2]))
